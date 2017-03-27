fa3ba82e60501c3210ba6a7095b0e0847261589e
José Francisco da Silva IV <silvaivctd@gmail.com>
2017-03-27 15:08:35 -0300

    Fix duplicate class Task #11 - RF - BiometriaService

--- src/Core/Application/Service/BiometriaService.php
+++ src/Core/Application/Service/BiometriaService.php
@@ -3,73 +3,19 @@ namespace OnyxERP\Core\Application\Service;
 
 use Exception;
 use Silex\Application;
-use \URL_BIOMETRIA_API;
+use URL_BIOMETRIA_API;
 
 /**
  * BiometraService.
  *
  * PHP version 5.6
  *
- * @author rinzler <github.com/feliphebueno>
- * @author jfranciscos4 <silvaivctd@gmail.com>
- * @copyright (c) 2007/2016, Grupo BRA - Solucoes para Gestao Publica
- * @license https://github.com/BRAConsultoria/Core/blob/master/LICENSE (c) 2007/2016, Grupo BRA - Solucoes para Gestao Publica
+ * @author    rinzler <github.com/feliphebueno>
+ * @author    jfranciscos4 <silvaivctd@gmail.com>
+ * @copyright (c) 2007/2017, Grupo BRA - Solucoes para Gestao Publica
+ * @license   https://github.com/BRAConsultoria/Core/blob/master/LICENSE Proprietary
  *
- * @version 1.5.1
- */
-class BiometriaService extends BaseService
-{
-
-    /**
-     *
-     * @return array
-     * @throws Exception Em caso de erro não tratável
-     */
-    public function getDadosBiometria($pfCod)
-    {
-        $conf = [
-            'body' => $this->getPayload(),
-            'exceptions' => false
-        ];
-
-        if (! empty($this->getJwt())) {
-            $conf['headers'] = [
-                'Authorization' => 'Bearer ' . $this->getJwt()
-            ];
-        }
-
-        $response = $this->app['guzzle']->get(URL_BIOMETRIA_API . 'full/' . $pfCod . '/', $conf);
-
-        try {
-            $decoded = \json_decode($response->getBody()->getContents(), true);
-        } catch (Exception $ex) {
-            $decoded = [];
-        }
-
-        if (isset($decoded['data']) === true) {
-            return $decoded['data'];
-        }
-
-        return [];
-    }
-}<?php
-namespace IpresbAPI\Core\Application\Service;
-
-use Exception;
-use Silex\Application;
-use \URL_BIOMETRIA_API;
-
-/**
- * BiometraService.
- *
- * PHP version 5.6
- *
- * @author rinzler <github.com/feliphebueno>
- * @author jfranciscos4 <silvaivctd@gmail.com>
- * @copyright (c) 2007/2016, Grupo BRA - Solucoes para Gestao Publica
- * @license https://github.com/BRAConsultoria/Core/blob/master/LICENSE (c) 2007/2016, Grupo BRA - Solucoes para Gestao Publica
- *
- * @version 1.5.1
+ * @version 1.1.0
  */
 class BiometriaService extends BaseService
 {
