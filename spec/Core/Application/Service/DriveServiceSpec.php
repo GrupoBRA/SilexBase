9fb81bf0a51829b71a58ed17a19a2ab841d8252a
Jose Francisco da Silva IV <silvaivctd@gmail.com>
2017-03-28 13:17:03 -0300

    implementação parcial Task #14 - RF - DriveService

--- spec/Core/Application/Service/DriveServiceSpec.php
+++ spec/Core/Application/Service/DriveServiceSpec.php
@@ -2,14 +2,35 @@
 
 namespace spec\OnyxERP\Core\Application\Service;
 
-use OnyxERP\Core\Application\Service\DriveService;
-use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use \OnyxERP\Core\Application\Service\DriveService;
+use \OnyxERP\Core\Domain\ValueObject\PfCod;
+use \PhpSpec\ObjectBehavior;
 
 class DriveServiceSpec extends ObjectBehavior
 {
+    /**
+     *
+     */
+    public function let()
+    {
+        chdir(__DIR__);
+        $app = require './../../../../bootstrap.php';
+        $this->beConstructedWith($app);
+    }
+    
     function it_is_initializable()
     {
         $this->shouldHaveType(DriveService::class);
+    }
+    
+    function it_get_dados_digitalizacao_com_sucesso()
+    {
+        $pfCod = new PfCod(1);
+        $lista = $this->getDadosDigitalizacao($pfCod);
+        $lista->shouldBeArray();
+        
+        $lista->shouldHaveKey('status');
+        $lista->shouldHaveKey('data');
+        
     }
 }
--- src/Core/Application/Service/DriveService.php
+++ src/Core/Application/Service/DriveService.php
@@ -2,6 +2,41 @@
 
 namespace OnyxERP\Core\Application\Service;
 
-class DriveService
-{
+use \Exception;
+use \OnyxERP\Core\Domain\ValueObject\PfCod;
+use const \URL_DRIVE_API;
+
+class DriveService extends BaseService
+{    
+    /**
+     * @return array
+     * @throws Exception Em caso de erro não tratável
+     */
+    public function getDadosDigitalizacao(PfCod $pfCod)
+    {
+        $conf = [
+                'body' => $this->getPayload(),
+                'exceptions' => false
+            ];
+
+        if (!empty($this->getJwt())) {
+            $conf['headers'] = [
+                    'Authorization' => 'Bearer ' . $this->getJwt(),
+                ];
+        }
+
+        $response = $this->app['guzzle']->get(URL_DRIVE_API . $pfCod->getValue() . '/modulo/7/', $conf);
+
+        try {
+            $decoded = \json_decode($response->getBody()->getContents(), true);
+        } catch (Exception $ex) {
+            $decoded = [];
+        }
+
+        if (isset($decoded['data']) === true) {
+            return $decoded['data'];
+        }
+            
+        return [];
+    }
 }
