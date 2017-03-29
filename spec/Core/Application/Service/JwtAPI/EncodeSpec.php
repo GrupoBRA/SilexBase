5fbea0ffdc0d04f112e7598608b48fd90d12c2e4
Jose Francisco da Silva IV <silvaivctd@gmail.com>
2017-03-29 13:51:57 -0300

    Melhorias no testes do PHPSpec Task #17 - RNF - Refatoração para organização dos serviços da APIs em pacotes individuais

--- spec/Core/Application/Service/JwtAPI/EncodeSpec.php
+++ spec/Core/Application/Service/JwtAPI/EncodeSpec.php
@@ -8,8 +8,33 @@ use Prophecy\Argument;
 
 class EncodeSpec extends ObjectBehavior
 {
+    /**
+     *
+     */
+    public function let()
+    {
+        chdir(__DIR__);
+        $app = require './../../../../../bootstrap.php';
+        $dados = [
+            "apiKey" => "NTdjOTc0ZjM3YzRmOA==",
+            "app" => [
+                "id" => "99",
+                "apikey" => "57c974f37c4f8",
+                "name" => "Dash"
+            ]
+        ];
+        $this->beConstructedWith($app, $dados);
+    }
+    /**
+     * 
+     */
     function it_is_initializable()
     {
         $this->shouldHaveType(Encode::class);
+    }
+    
+    function it_should_return_string()
+    {
+        $this->getResponse()->shouldBeString();
     }
 }
