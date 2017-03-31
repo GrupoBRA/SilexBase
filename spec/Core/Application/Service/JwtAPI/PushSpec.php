2f5c7a581ea263a3ae8c07731276b05d24a541d4
Jose Francisco da Silva IV <silvaivctd@gmail.com>
2017-03-31 15:13:37 -0300

    Finalização Task #24 - RF - Service JwtAPI/Push

--- spec/Core/Application/Service/JwtAPI/PushSpec.php
+++ spec/Core/Application/Service/JwtAPI/PushSpec.php
@@ -2,12 +2,30 @@
 
 namespace spec\OnyxERP\Core\Application\Service\JwtAPI;
 
-use OnyxERP\Core\Application\Service\JwtAPI\Push;
-use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use \OnyxERP\Core\Application\Service\JwtAPI\Encode;
+use \OnyxERP\Core\Application\Service\JwtAPI\Push;
+use \PhpSpec\ObjectBehavior;
 
 class PushSpec extends ObjectBehavior
 {
+    /**
+     *
+     */
+    public function let()
+    {
+        chdir(__DIR__);
+        $app = require './../../../../../bootstrap.php';
+        $dados = [
+            "apiKey" => "NTdjOTc0ZjM3YzRmOA==",
+            "app" => [
+                "id" => "99",
+                "apikey" => "57c974f37c4f8",
+                "name" => "Dash"
+            ]
+        ];
+        $jwt = (new Encode($app, $dados))->getResponse();
+        $this->beConstructedWith($app, $dados, $jwt);
+    }
     function it_is_initializable()
     {
         $this->shouldHaveType(Push::class);
