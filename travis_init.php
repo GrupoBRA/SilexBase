4e1ce4ec906feb4b9ea0abefe8a178dbab36cdb5
Silva IV <silvaivctd@gmail.com>
2017-04-05 22:05:27 -0300

    Update travis_init.php

--- travis_init.php
+++ travis_init.php
@@ -10,10 +10,10 @@ $template = <<<EOF
 <?php
         
 if (\defined('URL_AUTH_API') === false) {
-    \define('URL_AUTH_API', 'http://api.auth.alpha.onyxapis.com/v1/');
+    \define('URL_AUTH_API', 'http://auth-api.alpha.onyxapis.com/v1/');
 }
 if (\defined('URL_ACCOUNT_API') === false) {
-    \define('URL_ACCOUNT_API', 'http://api.account.alpha.onyxapis.com/v1/');
+    \define('URL_ACCOUNT_API', 'http://account-api.alpha.onyxapis.com/v1/');
 }
 if (\defined('URL_JWT_API') === false) {
     \define('URL_JWT_API', 'http://jwt-api.alpha.onyxapis.com/v1/');
@@ -21,7 +21,14 @@ if (\defined('URL_JWT_API') === false) {
 if (\defined('URL_APP_API') === false) {
     \define('URL_APP_API', 'http://app-api.alpha.onyxapis.com/v1/');
 }
-        
+if (\defined('URL_BIOMETRIA_API') === false) {
+    \define('URL_BIOMETRIA_API', 'http://biometria-api.alpha.onyxapis.com/v1/');
+}
+if (\defined('URL_DRIVE_API') === false) {
+    \define('URL_DRIVE_API', 'http://drive-api.alpha.onyxapis.com/v1/');
+}
+
+
 EOF;
 
 \file_put_contents($configPath . 'urls_apis.php', $template);
