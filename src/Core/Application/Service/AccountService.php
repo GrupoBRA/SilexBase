eee715234a55d0abf526fbc62f362f2f7363b502
Bueno, Feliphe <feliphezion@gmail.com>
2017-04-27 16:58:26 -0300

    Continução da issue #60
    Signed-off-by: Bueno, Feliphe <feliphezion@gmail.com>

--- src/Core/Application/Service/AccountService.php
+++ src/Core/Application/Service/AccountService.php
@@ -95,6 +95,55 @@ class AccountService
         }
     }
     
+    /**
+     * Envia um post ao end-point singup em AccountAPI
+     *
+     * @param string $cpf CPF do usuário a ser validado
+     * @return array Array com a resposta e o status code
+     * @throws Exception
+     */
+    public function confirmLogin($cpf)
+    {
+        try {
+
+            $conf = [
+                'connect_timeout' => 10,
+                'timeout' => 10,
+                'exceptions' => false
+            ];
+
+            if (!empty($this->getPayload())) {
+                $conf['body'] = \json_encode($this->getPayload());
+            }
+
+            if (!empty($this->getJwt())) {
+                $conf['headers'] = [
+                    'Authorization' => 'Bearer ' . $this->getJwt(),
+                ];
+            }
+
+            $response = $this->app['guzzle']->post(\URL_ACCOUNT_API . 'signup/confirm-login/'. $cpf .'/', $conf);
+
+            $responseText = $response->getBody()->getContents();
+
+            $this->app['monolog']->debug($responseText);
+
+            if ($response->getStatusCode() === 200) {
+                $responseObj = \json_decode($responseText, true);
+                $data = (isset($responseObj['data']) ? $responseObj['data'] : false);
+            } else {
+                $data = [];
+            }
+
+            return [
+                'status'    => $response->getStatusCode(),
+                'response'  => $data
+            ];
+        } catch (Exception $e){
+            throw new \RuntimeException($e->getMessage());
+        }
+    }
+    
     public function getJwt()
     {
         return $this->jwt;
