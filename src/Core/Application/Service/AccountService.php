f83af18eef98515c3197b389dc876294e610906e
Bueno, Feliphe <feliphezion@gmail.com>
2017-05-10 18:32:40 -0300

    De acordo com a issue #81
    Signed-off-by: Bueno, Feliphe <feliphezion@gmail.com>

--- src/Core/Application/Service/AccountService.php
+++ src/Core/Application/Service/AccountService.php
@@ -191,6 +191,55 @@ class AccountService
     }
     
     /**
+     * Envia um PUT ao end-point de reenvio do sms de validação em AccountAPI
+     *
+     * @param string $cpf CPF do usuário a ser validado
+     * @return array Array com a resposta e o status code
+     * @throws Exception
+     */
+    public function recoverySms($cpf)
+    {
+        try {
+
+            $conf = [
+                'connect_timeout' => 10,
+                'timeout' => 10,
+                'exceptions' => false
+            ];
+
+            if (!empty($this->getPayload())) {
+                $conf['body'] = \json_encode($this->getPayload());
+            }
+
+            if (!empty($this->getJwt())) {
+                $conf['headers'] = [
+                    'Authorization' => 'Bearer ' . $this->getJwt(),
+                ];
+            }
+
+            $response = $this->app['guzzle']->put(\URL_ACCOUNT_API . 'signup/recovery-sms/'. $cpf .'/', $conf);
+
+            $responseText = $response->getBody()->getContents();
+
+            $this->app['monolog']->debug($responseText);
+
+            if ($response->getStatusCode() === 200) {
+                $responseObj = \json_decode($responseText, true);
+                $data = (isset($responseObj['data']) ? $responseObj['data'] : false);
+            } else {
+                $data = [];
+            }
+
+            return [
+                'status'    => $response->getStatusCode(),
+                'response'  => $data
+            ];
+        } catch (Exception $e) {
+            throw new \RuntimeException($e->getMessage());
+        }
+    }
+    
+    /**
      * Envia um post para v2/login/ em AccountAPI. Requer JWT e payload data
      * 
      * @return array
